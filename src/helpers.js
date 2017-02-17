0ebe382367607d5c1d21c7147aa347e053ebf626
natew <natewienert@gmail.com>
2017-02-16 19:58:38 -0800

    :art: simplify to just basic view decorator

--- src/helpers.js
+++
@@ -1,7 +0,0 @@
-import { create } from 'mobx-persist'
-
-const decoratePersist = create({})
-
-export function persistStore(key, store) {
-  return key ? decoratePersist(key, store) : store
-}
--- src/index.js
+++ src/index.js
@@ -1,145 +1,69 @@
-import browser from 'detect-browser'
-import { observer } from 'mobx-react'
-import { observable } from 'mobx'
-import classHelpers, { CompositeDisposable } from 'macro-class-helpers'
-import mobxClassHelpers from 'mobx-class-helpers'
-import React from 'react'
-import { baseStyles } from 'macro-theme'
-import gloss from 'gloss'
-import developmentDecorate from './devDecorate'
-import type { AwesomeReactClass, Store } from './types'
-import { StoreCache } from 'motion-hmr'
-import { persistStore } from './helpers'
-
-export const val = observable
-export * from 'mobx'
-
-const styled = gloss({ baseStyles })
-// for store hmr
-const Cache = new StoreCache()
-// for dependency injection
-const injections = {}
-
-// @view decorator
-function view(
-  component: ReactClass<{}>,
-  provided: ?Object = null,
-  module?: Object,
-  plain?: boolean
-): AwesomeReactClass {
-  // hmr restore
-  if (module && module.hot) {
-    Cache.update(module, provided)
+function assertUndefined(parent, key) {
+  if (typeof parent[key] !== 'undefined') {
+    throw new Error(`Error, cannot overwrite existing property on class: ${key}`)
   }
-
-  class ProxyComponent {
-    constructor() {
-      this.subscriptions = new CompositeDisposable()
-      // inherit from React.Component
-      React.Component.apply(this, arguments)
-      component.apply(this, arguments)
-      // stores
-      if (provided) {
-        this.attachStores(provided)
-      }
-    }
-
-    // attaches stores provided to this instance
-    attachStores(provided: Object<string, Store>) {
-      const { storeKey } = this.props
-      const key = storeKey ? `${component.name}${storeKey}` : null
-      this.stores = Cache.restore(
-        this,
-        provided,
-        module,
-        store => persistStore(key, store)
-      )
-      for (const { key, store } of this.stores) {
-        // attach store directly to instance
-        if (this[key]) {
-          throw new Error(`Cannot set store to ${key}, component already has that defined: ${this[key]}`)
-        }
-        this[key] = store
-        // for debug, TODO: attach to a dev injection space, prevent name collision
-        if (this.app) {
-          this.app.stores[key] = store
-        }
-      }
-      // add dispose subscription
-      this.subscriptions.add(() => {
-        this.stores.forEach(({ store }) => {
-          if (store.dispose) {
-            store.dispose()
-          }
-        })
-      })
-    }
-
-    componentWillUnmount() {
-      this.subscriptions.dispose()
-      if (component.prototype.componentWillUnmount) {
-        component.prototype.componentWillUnmount.call(this)
-      }
-    }
-
-    // allow dependency injection into all classes, with some safety
-    inject(key, val) {
-      if (typeof this[key] !== 'undefined') {
-        throw new Error(`Can't defined ${key} onto class ${this.name}, already defined to ${this[key]}`)
-      }
-      injections[key] = val
-      Object.defineProperty(this, key, { get: () => injections[key] })
-    }
-
-    // render gets params
-    render() {
-      return super.render(this.props, this.state, this.context)
-    }
-  }
-
-  // proxy static properties
-  Object.keys(component).forEach(staticProp => {
-    ProxyComponent[staticProp] = component[staticProp]
-  })
-
-  // inherit class helpers
-  classHelpers(
-    ProxyComponent,
-    'ref',
-    'addEvent',
-    'setTimeout',
-    'setInterval',
-    'createCompositeDisposable'
-  )
-
-  // inherit mobx class helpers
-  mobxClassHelpers(
-    ProxyComponent,
-    'react',
-    'watch'
-  )
-
-  // inherit classes
-  Object.setPrototypeOf(component.prototype, React.Component.prototype)
-  Object.setPrototypeOf(ProxyComponent.prototype, component.prototype)
-
-  // add name
-  if (browser.name !== 'safari') {
-    Object.defineProperty(ProxyComponent, 'name', {
-      value: component.name,
-    })
-  }
-
-  if (process.env.NODE_ENV === "development") {
-    developmentDecorate(ProxyComponent)
-  }
-
-  return plain ?
-    styled(ProxyComponent) :
-    styled(observer(ProxyComponent))
 }
 
-view.provide = provided => module => component => view(component, provided, module)
-view.plain = component => view(component, false, false, true)
+// simple view decorator
+export default function instantiate(cb) {
+  const injections = {}
 
-export default view
+  function view(Klass) {
+    return cb ? cb(Klass) : Klass
+  }
+
+  // dead-simple dependency injection
+  view.inject = (injectables: Object) => {
+    for (const key of Object.keys(injectables)) {
+      if (typeof injections[key] !== 'undefined' && !(module && module.hot)) {
+        throw new Error(`Already injected ${key} into app`)
+      }
+      injections[key] = injectables[key]
+    }
+    return view
+  }
+
+  function getProvides(parent, provides) {
+    const result = {}
+    for (const key of provides) {
+      // attach injection
+      if (typeof key === 'string') {
+        if (typeof injections[key] === 'undefined') {
+          throw new Error(`Attempting to provide ${key} without first injecting, try @view.inject()`)
+        }
+        assertUndefined(parent, key)
+        result[key] = injections[key]
+      }
+      const provide = provides[key]
+      // attach object
+      if (provide instanceof Object) {
+        for (const subKey of Object.keys(provide)) {
+          assertUndefined(parent, subKey)
+          result[subKey] = provide[subKey]
+        }
+      }
+    }
+    return result
+  }
+
+  // attach object to class
+  const attachProvides = (Klass, provisions) => {
+    const provides = getProvides(Klass, provisions)
+    Object.keys(provides).forEach(key => {
+      Object.defineProperty(Klass.prototype, key, {
+        get: function() { return provides[key] }
+      })
+    })
+    return view(Klass)
+  }
+
+  view.provide = (...list) => moduleOrComponent => {
+    const isReactClass = typeof moduleOrComponent === 'function'
+    return isReactClass
+      ? view(moduleOrComponent)
+      // nest one more layer if we have motion-hmr transform
+      : Klass => attachProvides(Klass, list)
+  }
+
+  return view
+}
